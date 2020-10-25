9f660386c4f956d3650ad0a2bf0c1e592b20d639
balta-z-r <zunigaruizb@gmail.com>
2020-10-26 00:58:05 +0800

    fix: remove template name

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,19 +8,19 @@ module.exports = {
   siteMetadata: {
     title: `Baltazar Zuniga Ruiz`,
     author: `Baltazar Zuniga Ruiz`,
-    siteUrl: `https://balta-z-r.com/`
+    siteUrl: `https://balta-z-r.com/`,
   },
   plugins: [
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Baran Cezayirli | Technologist`,
+        name: `Baltazar Zuniga Ruiz`,
         short_name: `CV`,
         start_url: `/`,
         background_color: `#f7f0eb`,
         theme_color: `#38B2AC`,
         display: `standalone`,
-        icon: 'src/assets/site-icon.png'
+        icon: 'src/assets/site-icon.png',
       },
     },
     'gatsby-plugin-offline',
@@ -47,7 +47,7 @@ module.exports = {
       options: {
         printRejected: true,
         tailwind: true, // Enable tailwindcss support
-      }
-    }
+      },
+    },
   ],
 };
